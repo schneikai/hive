b2566d68a02446e48fd969161767d7af73b86ff2
Kai Schneider <schneikai@gmail.com>
2014-06-06 21:21:20 +0200

    read locale from a method

--- app/mailers/hive/mailer.rb
+++ app/mailers/hive/mailer.rb
@@ -2,23 +2,32 @@ class Hive::Mailer < Devise.parent_mailer.constantize
   include Devise::Mailers::Helpers
 
   def confirmation_instructions(record, token, opts={})
+    @record = record
     @token = token
-    I18n.with_locale(record.try(:locale) || I18n.locale) do
+    I18n.with_locale(locale) do
       devise_mail(record, :confirmation_instructions, opts)
     end
   end
 
   def reset_password_instructions(record, token, opts={})
+    @record = record
     @token = token
-    I18n.with_locale(record.try(:locale) || I18n.locale) do
+    I18n.with_locale(locale) do
       devise_mail(record, :reset_password_instructions, opts)
     end
   end
 
   def unlock_instructions(record, token, opts={})
+    @record = record
     @token = token
-    I18n.with_locale(record.try(:locale) || I18n.locale) do
+    I18n.with_locale(locale) do
       devise_mail(record, :unlock_instructions, opts)
     end
   end
+
+
+  protected
+    def locale
+      @record.try(:locale) || I18n.locale
+    end
 end
