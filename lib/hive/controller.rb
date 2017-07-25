bf52aac5279a286140475b73c18de0759c0d0d54
schneikai <schneikai@gmail.com>
2017-07-25 17:02:17 +0200

    fix after login redirect

--- lib/hive/controller.rb
+++ lib/hive/controller.rb
@@ -11,9 +11,17 @@ module Hive
       # error is raised. We rescue from that error and send to user to the login
       # page if not logged in or to the home page if already logged in but not
       # allowed to access the requested page.
+      #
+      # I added a crazy amount of code for login redirects via previous_location_for method.
+      # I don't know why I did this. It even doesn't work when just entering a url that requires
+      # authentication because then no referer is sent by the browser.
+      # I now save the current url before doing the redirect to new_user_session_url here. Simple!
       rescue_from CanCan::AccessDenied do |exception|
         respond_to do |format|
-          format.html { redirect_to (user_signed_in? ? main_app.root_url : main_app.new_user_session_url), alert: exception.message }
+          format.html do
+            store_location_for 'user', request.url
+            redirect_to (user_signed_in? ? main_app.root_url : main_app.new_user_session_url), alert: exception.message
+          end
           format.json { render text: exception.message, status: :forbidden }
         end
       end
